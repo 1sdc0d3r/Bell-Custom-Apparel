5103bf3ff2e2042da757261181540801ca04fe71
Braden <fish0859@gmail.com>
2024-02-26 18:14:32 -0700

    populating cards on blog home page

--- blog/blog-cards.js
+++ blog/blog-cards.js
@@ -14,18 +14,28 @@ for (let i = 0; i < articleList.length; i++) {
     const e = articleList[i];
     // console.log(e);
     const card = document.createElement("div");
+    card.className = 'card';
+    // card.href = ; //! link to article
+    const right = document.createElement("div");
+    right.className = 'right';
+
     const title = document.createElement("h1");
     const date = document.createElement("span");
     const author = document.createElement("span");
     author.classList.add("author");
     const snippet = document.createElement("p");
-    const content = document.createElement("pre");
+    const img = document.createElement("img");
+    img.src = `/images/blog/${e.imageUrl}`;
+    img.alt = e.imageAlt;
 
     title.textContent = e.title;
+    date.textContent = e.date;
     author.textContent = e.author;
-    // content.innerHTML = article.content;
+    snippet.textContent = e.snippet;
 
-    card.append(title, author, content);
+    right.append(title, author, date, snippet);
+    card.append(img, right);
+
     entryPoint.appendChild(card);
     console.log(e.title)
     console.log(document.querySelector("h1"));
