e9d04c9d90d891923a6c335c394ef3afef5a170c
Braden <fish0859@gmail.com>
2024-02-26 17:03:04 -0700

    working on blog pages with js

---
+++ blog/blog-card.js
@@ -0,0 +1,21 @@
+// id, title, author, date, imageUrl, imageAlt, category, tags, snippet, content
+
+const articleList = await import("./articles.js").then(res => res.default);
+const id = new URLSearchParams(window.location.search).get("id");
+const article = articleList.find(e => e.id == id)
+
+if (!article) window.location = "404.html"
+
+document.title = article.title + "| Blue Smoke Media"
+const entryPoint = document.querySelector(".blog");
+const title = document.createElement("h1");
+const author = document.createElement("span");
+const date = document.createElement("span");
+author.classList.add("author");
+const content = document.createElement("pre"); // or do a loop over the array of paragraphs
+
+title.textContent = article.title;
+author.textContent = article.author;
+content.innerHTML = article.content;
+
+entryPoint.append(title, author, content)
