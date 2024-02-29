aa4fb1b2f0db3287a7c2faba91484039cb7db12f
Braden <fish0859@gmail.com>
2024-02-29 12:16:58 -0700

    a2 server now connected to database

---
+++ API/server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+
+// app.js file.
+var express = require('express');
+var app = express();
+var mysql = require('mysql2');
+const cors = require("cors")
+app.use(cors())
+
+// console.log({
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE_NAME
+// })
+
+// 1. Require the connection to the database.
+var connection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+})
+connection.connect((err => {
+    if (err) throw err;
+    console.log('MySQL Connected');
+}));
+
+
+// 2. Make the GET requests.
+app.get('/api', (req, res) => {
+    let sql = 'SELECT * FROM Article';
+    const id = req.query.id;
+
+    if (id) sql += ` WHERE id=${id}`;
+
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        // console.log('get /')
+        id ? res.send(result[0]) :
+            res.send(result);
+    });
+});
+
+
+function getArticleById(req, res, id) {
+    let sql = `SELECT * FROM Article WHERE id=${id}`;
+
+    connection.query(sql, (err, result) => {
+        if (err) throw err
+        console.log(`get /${id}`);
+        res.send(result[0]);
+    });
+
+}
+
+app.listen(0, () => {
+    // console.log("server data ", server)
+    console.log(`Server listening.`);
+});
+// module.exports = app;
